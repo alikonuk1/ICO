dda4213315b72bb8ace7d8a6792a6e1928326228
ali konuk <alikonuk1@gmail.com>
2022-02-14 12:58:16 +0300

    1st

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,4 @@
+// Address of the NFT Contract that you deployed
+const CRYPTO_DEVS_NFT_CONTRACT_ADDRESS = "0x567e2c4a5FDf96Db5Fd552df13Caea9B5A0EF6b7";
+
+module.exports = { CRYPTO_DEVS_NFT_CONTRACT_ADDRESS };
